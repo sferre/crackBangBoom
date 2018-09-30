0dd202836d965e00c486a02f7155d89a466b0573
sferre <staniaferre0.sf@gmail.com>
2018-09-30 12:15:30 -0300

    Header Responsive - sin terminar

--- layout/header.php
+++ layout/header.php
@@ -1,33 +1,49 @@
 <header class="container-fluid">
 
   <!--NAV PRINCIPAL-->
-  <nav class="row nav1">
+  <div class="row justify-content-around nav1">
+<!-- ***** icono menu ***** -->
     <div id="nav-menu" class="">
-      <a href="#">
-        <img src="images/icons/menu.png" alt="menu">
-      </a>
+      <div class="menu">
+        <label for="menu-check" id="menu-toggle">
+          <img class="img-menu" src="images/icons/menu.png" alt="menu">
+        </label>
+        <!-- ***** checkbox ***** -->
+      <input type="checkbox" id="menu-check">
+      </div>
+
+      <!-- ***** nav ***** -->
+      <nav class="navigation">
+        <ul class="navigation-ul">
+            <li><a href="#">Sobre Nosotros</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Ingresar</a></li>
+          </ul>
+        </nav>
     </div>
+
+
+<!-- ***** logo ***** -->
     <div id="nav-logo" class="">
       <a href="#"><img src="images/icons/icon-cbb-02.svg" alt="CBB"></a>
     </div>
+<!-- ***** lupa ***** -->
     <div id="nav-search" class="">
       <a href="#">
         <img src="images/icons/search.png" alt="search">
       </a>
     </div>
-  </nav>
+
+  </div>
 
   <!--NAV SECUNDARIO-->
-  <nav class="row nav2">
-    <div class="btn-nav">
-      <a href="#">Ranking</a>
-    </div>
-    <div class="btn-nav">
-      <a href="#">Populares</a>
-    </div>
-    <div class="btn-nav">
-      <a href="#">Oferta</a>
-    </div>
+  <nav class="row justify-content-around nav2">
+    <ul class="row btn-nav">
+        <li class="col-4"><a href="#">Ranking</a></li>
+        <li class="col-4"><a href="#">Populares</a></li>
+        <li class="col-4"><a href="#">Oferta</a></li>
+    </ul>
   </nav>
 
 </header>
