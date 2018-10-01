2d648a5664b0de2268996ddb13f68de900eb8c60
Marina <muller.marina@hotmail.com>
2018-10-01 14:17:20 -0300

    Luchando con el banner

--- layout/banner.php
+++ layout/banner.php
@@ -1,10 +1,8 @@
 <div class="content">
 
-  
   <div class="welcome">
     <img src="images/icons/icon-cbb.svg" alt="">
   </div>
-
 
   <div class="dots">
     <img src="images/icons/dot.svg" alt="">
@@ -13,4 +11,5 @@
     <img src="images/icons/dot.svg" alt="">
     <img src="images/icons/dot.svg" alt="">
   </div>
+
 </div>
