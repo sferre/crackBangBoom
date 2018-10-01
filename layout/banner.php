3f51bc45380a5611432e6b360abfe13592ec1267
Marina <muller.marina@hotmail.com>
2018-10-01 19:42:36 -0300

    Avance con banner

--- layout/banner.php
+++ layout/banner.php
@@ -1,15 +1,17 @@
-<div class="content">
+<div class="row content">
 
-  <div class="welcome">
+  <div class="col-12 slide">
     <img src="images/icons/icon-cbb.svg" alt="">
   </div>
 
-  <div class="dots">
-    <a href="#"><img src="images/icons/dot.svg" alt=""></a>
-    <a href="#"><img src="images/icons/dot.svg" alt=""></a>
-    <a href="#"><img src="images/icons/dot.svg" alt=""></a>
-    <a href="#"><img src="images/icons/dot.svg" alt=""></a>
-    <a href="#"><img src="images/icons/dot.svg" alt=""></a>
+  <div class="col-12 go-bottom">
+    <div class="dots">
+      <a href="#"><img src="images/icons/dot.svg" alt=""></a>
+      <a href="#"><img src="images/icons/dot.svg" alt=""></a>
+      <a href="#"><img src="images/icons/dot.svg" alt=""></a>
+      <a href="#"><img src="images/icons/dot.svg" alt=""></a>
+      <a href="#"><img src="images/icons/dot.svg" alt=""></a>
+    </div>
   </div>
 
 </div>
