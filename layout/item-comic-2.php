03bd50e2211de1a9ac672b3fb88bb9adb831fad1
Marina <muller.marina@hotmail.com>
2018-10-01 23:07:24 -0300

    Estilo distinto hecho para Sección Doble
    falta foto

--- layout/item-comic-2.php
+++ layout/item-comic-2.php
@@ -1,14 +1,16 @@
 <div class="item">
-  <a>
+
 <!-- ***** Portada Comic ***** -->
     <div class="cover">
-      <img src="./images/covers/wonder-woman-1.jpg" alt="">
+      <a>
+      <!--<img src="./images/covers/wonder-woman-1.jpg" alt="">-->
       <!-- <img src="" alt="oferta"> -->
+      </a>
     </div>
 <!-- ***** Info Comic ***** -->
     <div class="info">
         <div class="title">
-          <p>Wonder Woman</p>
+          <a><p>Wonder Woman</p></a>
         </div>
         <div class="edition">
           <p>ISSUE #01</p>
@@ -17,5 +19,5 @@
           <p>$99.99</p>
         </div>
     </div>
-  </a>
+
 </div>
