319d8405b23bb1a62b3c5ccd3ef6f74d32238de7
Marina <muller.marina@hotmail.com>
2018-10-03 20:02:13 -0300

    Arreglos en FAQ y SobreNosotros

--- faq.php
+++ faq.php
@@ -22,120 +22,123 @@
 
     <div class="container-fluid contact-principal">
 
-      <section class="faq-content">
+      <section class="align-content">
 
-        <!--Titulo -->
-        <div class="row titulo-contact">
-          <h1 class="col-12">Preguntas Frecuentes</h1>
-          <p class="col-12">
-          <i>"Dios lo sabe todo excepto la combinación de la caja fuerte."</i>
-          </p>
-        </div>
+        <section class="faq-content">
+
+          <!--Titulo -->
+          <div class="row titulo-contact">
+            <h1 class="col-12">Preguntas Frecuentes</h1>
+            <p class="col-12">
+            <i>"Dios lo sabe todo excepto la combinación de la caja fuerte."</i>
+            </p>
+          </div>
 
 
-  			<div>
-  			<h2 class="titulosPrin">Sobre la pagina</h2>
+    			<div>
+    			<h2 class="titulosPrin">Sobre la pagina</h2>
 
-        <div class="preguntas">
-  				<h2>¿Qué es la pagina?</h2>
-  				<div>
-  					<p>
-  						Somos una Plataforma de Comincs que te permite comprar online con tarjeta de crédito y ofrecer cuotas. Podes accedés a todos los medios de pago adheridos, mediante una única conexión y un único contrato. Si querés pagar, te ofrece hacerlo en cuotas con todas las tarjetas de crédito y débito adheridas y crear tu billetera virtual para que no tengas que ingresar todos los datos de tus tarjetas cada vez que realices una compra.
-  					</p>
-  				</div>
-  			</div>
+          <div class="preguntas">
+    				<h2>¿Qué es la pagina?</h2>
+    				<div>
+    					<p>
+    						Somos una Plataforma de Comincs que te permite comprar online con tarjeta de crédito y ofrecer cuotas. Podes accedés a todos los medios de pago adheridos, mediante una única conexión y un único contrato. Si querés pagar, te ofrece hacerlo en cuotas con todas las tarjetas de crédito y débito adheridas y crear tu billetera virtual para que no tengas que ingresar todos los datos de tus tarjetas cada vez que realices una compra.
+    					</p>
+    				</div>
+    			</div>
 
-        <div class="preguntas">
-  				<h2>¿Como accedo al servicio?</h2>
-  				<div>
-  					<p>
-  						Para acceder al servicio, deberás ingresar en www.link.com.ar y registrarte. Podés crear tu cuenta como comprador y/o para vender.
-  					</p>
-  				</div>
-  			</div>
-  			</div>
-  			<div>
-  				<h2 class="titulosPrin">Registracion al servicio</h2>
-  				<div class="preguntas">
-  					<h2>Quiero vender ¿como me registro?</h2>
-  					<div>
-  						<p>
-  							Si sos un Vendedor Eventual: registrate con una Cuenta Persona, completá tus datos y confirmá tu e-mail.
+          <div class="preguntas">
+    				<h2>¿Como accedo al servicio?</h2>
+    				<div>
+    					<p>
+    						Para acceder al servicio, deberás ingresar en www.link.com.ar y registrarte. Podés crear tu cuenta como comprador y/o para vender.
+    					</p>
+    				</div>
+    			</div>
+    			</div>
+    			<div>
+    				<h2 class="titulosPrin">Registracion al servicio</h2>
+    				<div class="preguntas">
+    					<h2>Quiero vender ¿como me registro?</h2>
+    					<div>
+    						<p>
+    							Si sos un Vendedor Eventual: registrate con una Cuenta Persona, completá tus datos y confirmá tu e-mail.
 
-  							Si sos un Profesional Independiente: registrate con una Cuenta Persona, indicando tu CUIT y condición fiscal. Recordá ingresar la información necesaria para que podamos facturarte de acuerdo a tu situación tributaria, podrás hacerlo desde la Sección Perfil – Datos de Facturación.
+    							Si sos un Profesional Independiente: registrate con una Cuenta Persona, indicando tu CUIT y condición fiscal. Recordá ingresar la información necesaria para que podamos facturarte de acuerdo a tu situación tributaria, podrás hacerlo desde la Sección Perfil – Datos de Facturación.
 
-  							Si tenes una Empresa: registrate con una Cuenta Empresa, completá los datos y confirmá tu e-mail. Recordá ingresar la documentación comercial e información de facturación necesaria para empezar a operar, podrás hacerlo desde la Sección Perfil – Datos de Facturación.
-  						</p>
-  					</div>
-  				</div>
-  				<div class="preguntas">
-  					<h2>Quiero crear mi Billetera Virtual ¿como me registro?</h2>
-  					<div>
-  						<p>
-  							Te podes adherir ingresando en www.link.com.ar, realizás el proceso de registración y una vez habilitada la cuenta, en la sección Billetera, podrá adherir todos sus medios de pago. Deberás ingresar manualmente y por única vez todos los datos de las tarjetas de crédito y débito que desees adherir. Nosotros validaremos tu titularidad.
-  						</p>
-  					</div>
-  				</div>
-  			</div>
-  			<div>
-  				<h2 class="titulosPrin">Mi Cuenta</h2>
-  				<div class="preguntas">
-  					<h2>Como puedo acceder a mi cuenta?</h2>
-  					<div>
-  						<p>
-  							Para ingresar a tu cuenta, deberás ingresar en www.link.com y hacer click en el botón Ingresar, ubicado en el margen superior derecho.
-  						</p>
-  					</div>
-  				</div>
-  				<div class="preguntas">
-  					<h2>No puedo acceder a mi cuenta ¿Que hago?</h2>
-  					<div>
-  						<p>
-  							Si el motivo por el cual no podés acceder es  porque olvidaste o bloqueaste tu contraseña, deberás ingresar tu mail y hacer click en  “¿Olvidaste o bloqueaste tu contraseña? Ingresá aquí” de la pantalla de ingreso a la pagina. Recibirás un correo para recuperarla.
-  							Si no podés ingresar por algún otro motivo contactarte con nuestro Centro de Atención telefónica al 0-800-000-0000
-  						</p>
-  					</div>
-  				</div>
-  				<div class="preguntas">
-  					<h2>Mi cuenta esta bloqueada ¿Como la habilito?</h2>
-  					<div>
-  						<p>
-  							Luego de cinco intentos fallidos de intentar ingresar a tu cuenta de esta pagina tu contraseña será bloqueada y recibirás un correo con la opción de recuperarla. Deberás completar la pregunta de seguridad, ingresar una nueva clave y quedará restablecida. Si no recordás tu contraseña, llamanos al 0-800-000-0000 (de lunes a viernes de 8 a 24hs. o sábados y domingos de 10 a 18hs).También podés escribirnos por mensaje directo a nuestras Redes Sociales:
-  							Facebook: https://www.facebook.com/pagina
-  							Twitter: https://twitter.com/pagina
-  						</p>
-  					</div>
-  				</div>
-  				<div class="preguntas">
-  					<h2>Expiro mi cuenta ¿Como creo una nueva?</h2>
-  					<div>
-  						<p>
-  							Cuando expire tu contraseña recibirás un correo electrónico con el aviso y la opción de renovarla. Deberás completar la pregunta de seguridad, ingresar una nueva clave y quedará restablecida. Si no recordás tu contraseña, llamanos al 0-800-000-0000 (de lunes a viernes de 8 a 24hs. o sábados y domingos de 10 a 18hs).También podés escribirnos por mensaje directo a nuestras Redes Sociales:
-  							Facebook: https://www.facebook.com/pagina
-  							Twitter: https://twitter.com/pagina
-  						</p>
-  					</div>
-  				</div>
-  				<div class="preguntas">
-  					<h2>Me registre y quiero convertirme en Usuario Profesional ¿Como hago?</h2>
-  					<div>
-  						<p>
-  							Si al momento de registrarte no indicaste tu CUIT ni los datos de facturacion, solo tenes que ingresar a tu Cuenta, en la seccion Perfil - Datos de Facturacion solitita cambiar tu cuenta a Usuario Profesional y segui las intrucciones.
-  							Cabiarte a Usuario Profesional te permitira, entre otras cosas, aumentas los limites de tus cobros.
-  						</p>
-  					</div>
-  				</div>
-  				<div class="preguntas">
-  					<h2>Cambie mi condicion fiscal ¿Que debo hacer?</h2>
-  					<div>
-  						<p>
-  							Deberás actualizar tu condición fiscal en la Sección Perfil > Datos de Facturación. Recordá adjuntar la documentación que te solicitaremos.
-  						</p>
-  					</div>
-  				</div>
+    							Si tenes una Empresa: registrate con una Cuenta Empresa, completá los datos y confirmá tu e-mail. Recordá ingresar la documentación comercial e información de facturación necesaria para empezar a operar, podrás hacerlo desde la Sección Perfil – Datos de Facturación.
+    						</p>
+    					</div>
+    				</div>
+    				<div class="preguntas">
+    					<h2>Quiero crear mi Billetera Virtual ¿como me registro?</h2>
+    					<div>
+    						<p>
+    							Te podes adherir ingresando en www.link.com.ar, realizás el proceso de registración y una vez habilitada la cuenta, en la sección Billetera, podrá adherir todos sus medios de pago. Deberás ingresar manualmente y por única vez todos los datos de las tarjetas de crédito y débito que desees adherir. Nosotros validaremos tu titularidad.
+    						</p>
+    					</div>
+    				</div>
+    			</div>
+    			<div>
+    				<h2 class="titulosPrin">Mi Cuenta</h2>
+    				<div class="preguntas">
+    					<h2>Como puedo acceder a mi cuenta?</h2>
+    					<div>
+    						<p>
+    							Para ingresar a tu cuenta, deberás ingresar en www.link.com y hacer click en el botón Ingresar, ubicado en el margen superior derecho.
+    						</p>
+    					</div>
+    				</div>
+    				<div class="preguntas">
+    					<h2>No puedo acceder a mi cuenta ¿Que hago?</h2>
+    					<div>
+    						<p>
+    							Si el motivo por el cual no podés acceder es  porque olvidaste o bloqueaste tu contraseña, deberás ingresar tu mail y hacer click en  “¿Olvidaste o bloqueaste tu contraseña? Ingresá aquí” de la pantalla de ingreso a la pagina. Recibirás un correo para recuperarla.
+    							Si no podés ingresar por algún otro motivo contactarte con nuestro Centro de Atención telefónica al 0-800-000-0000
+    						</p>
+    					</div>
+    				</div>
+    				<div class="preguntas">
+    					<h2>Mi cuenta esta bloqueada ¿Como la habilito?</h2>
+    					<div>
+    						<p>
+    							Luego de cinco intentos fallidos de intentar ingresar a tu cuenta de esta pagina tu contraseña será bloqueada y recibirás un correo con la opción de recuperarla. Deberás completar la pregunta de seguridad, ingresar una nueva clave y quedará restablecida. Si no recordás tu contraseña, llamanos al 0-800-000-0000 (de lunes a viernes de 8 a 24hs. o sábados y domingos de 10 a 18hs).También podés escribirnos por mensaje directo a nuestras Redes Sociales:
+    							Facebook: https://www.facebook.com/pagina
+    							Twitter: https://twitter.com/pagina
+    						</p>
+    					</div>
+    				</div>
+    				<div class="preguntas">
+    					<h2>Expiro mi cuenta ¿Como creo una nueva?</h2>
+    					<div>
+    						<p>
+    							Cuando expire tu contraseña recibirás un correo electrónico con el aviso y la opción de renovarla. Deberás completar la pregunta de seguridad, ingresar una nueva clave y quedará restablecida. Si no recordás tu contraseña, llamanos al 0-800-000-0000 (de lunes a viernes de 8 a 24hs. o sábados y domingos de 10 a 18hs).También podés escribirnos por mensaje directo a nuestras Redes Sociales:
+    							Facebook: https://www.facebook.com/pagina
+    							Twitter: https://twitter.com/pagina
+    						</p>
+    					</div>
+    				</div>
+    				<div class="preguntas">
+    					<h2>Me registre y quiero convertirme en Usuario Profesional ¿Como hago?</h2>
+    					<div>
+    						<p>
+    							Si al momento de registrarte no indicaste tu CUIT ni los datos de facturacion, solo tenes que ingresar a tu Cuenta, en la seccion Perfil - Datos de Facturacion solitita cambiar tu cuenta a Usuario Profesional y segui las intrucciones.
+    							Cabiarte a Usuario Profesional te permitira, entre otras cosas, aumentas los limites de tus cobros.
+    						</p>
+    					</div>
+    				</div>
+    				<div class="preguntas">
+    					<h2>Cambie mi condicion fiscal ¿Que debo hacer?</h2>
+    					<div>
+    						<p>
+    							Deberás actualizar tu condición fiscal en la Sección Perfil > Datos de Facturación. Recordá adjuntar la documentación que te solicitaremos.
+    						</p>
+    					</div>
+    				</div>
 
-  		</div>
+    		  </div>
 
+        </section>
 
       </section>
 
--- sobre-nosotros.php
+++ sobre-nosotros.php
@@ -22,42 +22,45 @@
 
     <div class="container-fluid contact-principal">
 
-      <section class="faq-content">
+      <section class="align-content">
 
-        <!--Titulo -->
-        <div class="row titulo-contact">
-          <h1 class="col-12">SOBRE NOSOTROS...</h1>
-          <p class="col-12">
-          <i>"Todos los superhéroes tienen una identidad secreta. No sé de alguno que no la tenga.
-            ¿Quién quiere la presión de ser superhéroe todo el tiempo?"
-          Mr.Increible</i>
-          </p>
-        </div>
+        <section class="faq-content">
+
+          <!--Titulo -->
+          <div class="row titulo-contact">
+            <h1 class="col-12">SOBRE NOSOTROS</h1>
+            <p class="col-12">
+            <i>"Todos los superhéroes tienen una identidad secreta. No sé de alguno que no la tenga.
+              ¿Quién quiere la presión de ser superhéroe todo el tiempo?"
+            Mr.Increible</i>
+            </p>
+          </div>
 
 
-  			<div>
-  			<h2 class="titulosPrin"></h2>
+    			<div>
 
-        <div class="preguntas">
-  				<h2>¿QUIÉNES SOMOS?</h2>
-  				<div>
-  					<p>
-  						Somos un grupo de estudiantes que disfrutan de las buenas historias...y si tienen bellas imagenes, mejor!
-              Estamos buscando promover la industria del comic en Argentina y aprobar nuestro primer sprint.
-  					</p>
-  				</div>
-  			</div>
+          <div class="preguntas">
+    				<h2>¿QUIÉNES SOMOS?</h2>
+    				<div>
+    					<p>
+    						Somos un grupo de estudiantes que disfrutan de las buenas historias...y si tienen bellas imagenes, mejor!
+                Estamos buscando promover la industria del comic en Argentina y aprobar nuestro primer sprint.
+    					</p>
+    				</div>
+    			</div>
 
-        <div class="row-justify-content-center">
+          <div class="row-justify-content-center">
 
-  					<p class="col-10">
-  			      Nuestros héroes nos inspiran a mejorar día a día. Convertite en uno y decinos cómo podemos ser extraordinarios <a class="link-contacto" href="contact.php">¡CONTACTANOS!</a>
-  					</p>
-  			</div>
-  			</div>
+    					<p class="col-10">
+    			      Nuestros héroes nos inspiran a mejorar día a día. Convertite en uno y decinos cómo podemos ser extraordinarios <a class="link-contacto" href="contact.php">¡CONTACTANOS!</a>
+    					</p>
+    			</div>
+    			</div>
 
-  		</div>
+    		</div>
 
+
+        </section>
 
       </section>
 
