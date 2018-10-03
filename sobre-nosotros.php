d2aff4c3b63230843237f520abb89beab9eb2569
sferre <stefaniaferre0.sf@gmail.com>
2018-10-03 10:38:20 -0300

    Validaciones de Contacto + Pagina de Sobre-nosotros.php

---
+++ sobre-nosotros.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <title>Crack Bang Boom! - Preguntas Frecuentes</title>
+    <link href="css/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+    <link rel="stylesheet" href="css/main-style.css">
+    <link rel="stylesheet" href="css/contact-style.css">
+    <link rel="stylesheet" type="text/css" href="css/faq-style.css">
+  </head>
+  <body>
+
+    <div id="desktop-container">
+
+      <!--HEADER-->
+      <?php include('layout/header.php'); ?>
+
+    </div>
+
+    <div class="container-fluid contact-principal">
+
+      <section class="faq-content">
+
+        <!--Titulo -->
+        <div class="row titulo-contact">
+          <h1 class="col-12">SOBRE NOSOTROS...</h1>
+          <p class="col-12">
+          <i>"Todos los superhéroes tienen una identidad secreta. No sé de alguno que no la tenga.
+            ¿Quién quiere la presión de ser superhéroe todo el tiempo?"
+          Mr.Increible</i>
+          </p>
+        </div>
+
+
+  			<div>
+  			<h2 class="titulosPrin"></h2>
+
+        <div class="preguntas">
+  				<h2>¿QUIÉNES SOMOS?</h2>
+  				<div>
+  					<p>
+  						Somos un grupo de estudiantes que disfrutan de las buenas historias...y si tienen bellas imagenes, mejor!
+              Estamos buscando promover la industria del comic en Argentina y aprobar nuestro primer sprint.
+  					</p>
+  				</div>
+  			</div>
+
+        <div class="row-justify-content-center">
+
+  					<p class="col-10">
+  			      Nuestros héroes nos inspiran a mejorar día a día. Convertite en uno y decinos cómo podemos ser extraordinarios <a class="link-contacto" href="contact.php">¡CONTACTANOS!</a>
+  					</p>
+  			</div>
+  			</div>
+
+  		</div>
+
+
+      </section>
+
+    </div>
+
+    <div id="desktop-container">
+
+      <!--FOOTER-->
+      <div class="container-fluid">
+        <?php include('layout/footer.php'); ?>
+      </div>
+
+    </div>
+
+  </body>
+</html>
